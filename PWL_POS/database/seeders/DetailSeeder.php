01db0d0d33ef3df1c719f14f9b7f38fb44449692
drm69 <yukihira14darma@gmail.com>
2025-03-06 20:46:50 +0700

    jobsheet03

--- PWL_POS/database/seeders/DetailSeeder.php
+++ PWL_POS/database/seeders/DetailSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DetailSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class DetailSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [];
+
+        for ($i = 50; $i <= 59; $i++) {
+            for ($j=1; $j < 4; $j++) { 
+                $data[] = [
+                    'penjualan_id' => ($i-10),
+                    'barang_id' => ($i-30),
+                    'harga' => 50000,
+                    'jumlah' => $j,
+                ];
+            }
+        }
+        DB::table('t_penjualan_detail')->insert($data);
     }
 }
--- PWL_POS/database/seeders/StokSeeder.php
+++ PWL_POS/database/seeders/StokSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class StokSeeder extends Seeder
 {
@@ -12,6 +14,78 @@ class StokSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'stok_id' => 30,
+                'barang_id' => 20,
+                'user_id' => 1,
+                'stok_tanggal' => now(),
+                'stok_jumlah' => 24,
+            ],
+            [
+                'stok_id' => 31,
+                'barang_id' => 22,
+                'user_id' => 1,
+                'stok_tanggal' => now(),
+                'stok_jumlah' => 20,
+            ],
+            [
+                'stok_id' => 32,
+                'barang_id' => 23,
+                'user_id' => 1,
+                'stok_tanggal' => now(),
+                'stok_jumlah' => 12,
+            ],
+            [
+                'stok_id' => 33,
+                'barang_id' => 24,
+                'user_id' => 1,
+                'stok_tanggal' => now(),
+                'stok_jumlah' => 33,
+            ],
+            [
+                'stok_id' => 34,
+                'barang_id' => 25,
+                'user_id' => 1,
+                'stok_tanggal' => now(),
+                'stok_jumlah' => 32,
+            ],
+            [
+                'stok_id' => 35,
+                'barang_id' => 26,
+                'user_id' => 1,
+                'stok_tanggal' => now(),
+                'stok_jumlah' => 11,
+            ],
+            [
+                'stok_id' => 36,
+                'barang_id' => 27,
+                'user_id' => 1,
+                'stok_tanggal' => now(),
+                'stok_jumlah' => 9,
+            ],
+            [
+                'stok_id' => 37,
+                'barang_id' => 28,
+                'user_id' => 1,
+                'stok_tanggal' => now(),
+                'stok_jumlah' => 23,
+            ],
+            [
+                'stok_id' => 38,
+                'barang_id' => 29,
+                'user_id' => 1,
+                'stok_tanggal' => now(),
+                'stok_jumlah' => 26,
+            ],
+            [
+                'stok_id' => 39,
+                'barang_id' => 21,
+                'user_id' => 1,
+                'stok_tanggal' => now(),
+                'stok_jumlah' => 28,
+            ],
+        ];
+        DB::table('t_stok') -> insert($data);
     }
 }
