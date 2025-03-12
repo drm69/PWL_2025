725b6f5feaa6e58d6b6a3d27b41af7d2dbb9d2b5
drm69 <yukihira14darma@gmail.com>
2025-03-13 01:08:52 +0700

    – Retreiving or Creating Models

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -26,7 +26,20 @@ class UserController extends Controller
         //$user = UserModel::findOrFail(1);
         //return view('user', ['data' => $user]);
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        //$user = UserModel::where('username', 'manager9')->firstOrFail();
+        //return view('user', ['data' => $user]);
+
+        //$user = UserModel::where('level_id', 1)->count();
+
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2,
+            ],
+        );
+        $user->save();
         return view('user', ['data' => $user]);
     }
 }
