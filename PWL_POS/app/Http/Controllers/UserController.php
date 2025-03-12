769fe09748e23517e3f591e33f1fd4aa863a3f2e
drm69 <yukihira14darma@gmail.com>
2025-03-13 00:13:58 +0700

    Not Found Exceptions

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -9,12 +9,24 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $data  = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data);
+        //$user = UserModel::find(1);
+        //return view('user', ['data' => $user]);
+        
+        //$user = UserModel::where('level_id', 1)->first();
+        //return view('user', ['data' => $user]);
 
-        $user = UserModel::all();
+        //$user = UserModel::firstWhere('level_id', 1);
+        //return view('user', ['data' => $user]);
+
+        /*$user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);*/
+
+        //$user = UserModel::findOrFail(1);
+        //return view('user', ['data' => $user]);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
--- PWL_POS_M4/app/Http/Controllers/UserController.php
+++ PWL_POS_M4/app/Http/Controllers/UserController.php
@@ -9,15 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $data  = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
-
-        $user = UserModel::all();
+        $user = UserModel::firstWhere('level_id', 1);
         return view('user', ['data' => $user]);
     }
 }
