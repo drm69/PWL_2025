63525a7ed15227802d5ca4d082b38ce7eab08164
drm69 <yukihira14darma@gmail.com>
2025-03-13 20:38:28 +0700

    Create, Read, Update, Delete (CRUD)

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -6,7 +6,7 @@
     <title>Data User</title>
 </head>
 <body>
-    <h1>Data Kategori Barang</h1>
+    <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
@@ -14,12 +14,16 @@
             <th>Nama</th>
             <th>ID Level Pengguna</th>
         </tr>
+        @foreach ($data as $d)
         <tr>
-            <td>{{$data->user_id}}</td>
-            <td>{{$data->username}}</td>
-            <td>{{$data->nama}}</td>
-            <td>{{$data->level_id}}</td>
+            <td>{{$d->user_id}}</td>
+            <td>{{$d->username}}</td>
+            <td>{{$d->nama}}</td>
+            <td>{{$d->level_id}}</td>
+            <td><a href="{{route('ubah', $d)}}">Ubah</a> | <a href="{{route('hapus', $d)}}">Hapus</a></td>
         </tr>
+        @endforeach
     </table>
+    <a href="{{route('tambah')}}">tambah user</a>
 </body>
 </html>
