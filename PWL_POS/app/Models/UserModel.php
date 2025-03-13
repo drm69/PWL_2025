bc458965ae91558b2a83c17df7a7f8dcbb7890f9
drm69 <yukihira14darma@gmail.com>
2025-03-13 21:22:20 +0700

    Relationships

--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -4,16 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
     use HasFactory;
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id';
-    protected $fillable = [
-        'username',
-        'nama',
-        'password',
-        'level_id',
-    ];
+
+    protected $table = 'm_user'; // Nama tabel
+    protected $primaryKey = 'user_id'; // Primary key
+    public $timestamps = true; // Jika tabel memiliki created_at & updated_at
+
+    protected $fillable = ['username', 'nama', 'password', 'level_id'];
+
+    // Relasi ke LevelModel
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
