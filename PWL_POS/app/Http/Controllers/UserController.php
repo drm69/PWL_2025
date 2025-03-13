825278ed93bccdd48f1e624d8f5a8893954b9c5e
drm69 <yukihira14darma@gmail.com>
2025-03-13 14:53:11 +0700

    Attribute Changes

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -9,37 +9,21 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        //$user = UserModel::find(1);
-        //return view('user', ['data' => $user]);
-        
-        //$user = UserModel::where('level_id', 1)->first();
-        //return view('user', ['data' => $user]);
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
 
-        //$user = UserModel::firstWhere('level_id', 1);
-        //return view('user', ['data' => $user]);
+        $user->username = 'manager12';
 
-        /*$user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-        return view('user', ['data' => $user]);*/
-
-        //$user = UserModel::findOrFail(1);
-        //return view('user', ['data' => $user]);
-
-        //$user = UserModel::where('username', 'manager9')->firstOrFail();
-        //return view('user', ['data' => $user]);
-
-        //$user = UserModel::where('level_id', 1)->count();
-
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ],
-        );
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
     }
 }
