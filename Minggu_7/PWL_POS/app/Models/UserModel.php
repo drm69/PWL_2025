b419115eccd1544614f45cad9470617240ad016a
drm69 <yukihira14darma@gmail.com>
2025-04-18 00:29:05 +0700

    jobsheet 7

--- Minggu_7/PWL_POS/app/Models/UserModel.php
+++ Minggu_7/PWL_POS/app/Models/UserModel.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\Level;
 use App\Models\LevelModel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
@@ -17,8 +17,31 @@ class UserModel extends Model
 
     protected $fillable = ['level_id', 'username','password', 'nama'];
 
+    protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed'];
+
+    /**
+     * Relasi ke tabel level
+     */
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+
+    /**
+     * Mendapatkan nama role
+     */
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    /**
+     * Cek apakah user memiliki role tertentu
+     */
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
+    }
+
 }
